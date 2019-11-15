eabeb2b92499daf0815c8bcefdc5c6074e7a1e3f
Anthony Brown <groundup2360917182914017@gmail.com>
2019-11-15 14:13:39 -0500

    Add to not save some months per year

--- monthstake1.py
+++ monthstake1.py
@@ -3,22 +3,28 @@
 import sys
 import math
 
-if len(sys.argv) == 3:
-    number1 = float(sys.argv[2])
+if len(sys.argv) == 4:
+	number1 = float(sys.argv[2])
 
-    total1 = float(sys.argv[1])
+	total1 = float(sys.argv[1])
 
-    timetakes = total1 / number1
+	timetakes = total1 / number1
 
-    years = int(timetakes / 12)
+	years = int(timetakes / 12)
 
-    months = int(math.ceil(timetakes % 12))
+	months = int(math.ceil(timetakes % 12))
 
-    if months == 12:
-        months = 0
-        years = years + 1
+	if months == 12:
+		months = 0
+		years = years + 1
 
-    print("The total amount of years and months to produce $", total1, "saving $", number1, "per month is", years, "years and", months, "months")
+	months = months + int(sys.argv[3]) * years
+
+	years = years + int(months / 12)
+
+	months = int(months % 12)
+
+	print("The total amount of years and months to produce $", total1, "saving $", number1, "per month and not saving $", number1, "for", int(sys.argv[3]), "times per year is", years, "years and", months, "months")
 else:
-    print(sys.argv[0], "<total price> <money saved per month>")
+	print(sys.argv[0], "<total price> <money saved per month> <months cant save anything>")
 

54a065f2e9040ee59058357bf2e809de5747fca7
Anthony Brown <groundup2360917182914017@gmail.com>
2019-11-15 16:01:30 -0500

    Correct when months not saved

--- monthstake1.py
+++ monthstake1.py
@@ -8,17 +8,11 @@ if len(sys.argv) == 4:
 
 	total1 = float(sys.argv[1])
 
-	moneyoff = float(sys.argv[3])
+	moneyoff = int(sys.argv[3])
 
-	timetakes = total1 / number1
+	months = int(math.ceil((total1 / number1) % (12 - moneyoff)))
 
-	years = int(timetakes / 12)
-
-	total2 = total1 / number1 + (years+1) * moneyoff
-
-	months = int(math.ceil(total2 % 12))
-
-	years = int(total2 / 12)
+	years = int((total1 / number1) / (12 - moneyoff))
 
 	print("The total amount of years and months to produce $", total1, "saving $", number1, "per month and not saving $", number1, "for", int(sys.argv[3]), "times per year is", years, "years and", months, "months")
 else:
